0f2b9bcd530e4c496d02c86ca072f3259f969cd2
Yuriy Derevenko <yuriyderevenko@MB-277172.local>
2022-06-06 05:54:49 +0300

    refactor: add tooling (eslint, nyc, winston logger, pre-push hook)

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8010;
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
 
-const buildSchemas = require('./src/schemas');
+const buildSchemas = require("./src/schemas");
+const { logger } = require("./src/utils");
 
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db);
 
-    const app = require('./src/app')(db);
+  const app = require("./src/app")(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+  app.listen(port, () =>
+    logger.info(`App started and listening on port ${port}`)
+  );
 });
